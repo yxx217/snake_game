1baf5c52b506e411a13d6699363f59603220d84d
yxx217 <m-10742662@moe-dl.edu.my>
2024-08-26 16:17:36 +0800

    Update grid.js

--- grid.js
+++ grid.js
@@ -2,8 +2,8 @@ const GRID_SIZE = 21
 
 export function randomGridPosition() {
   return {
-    x: Math.floor(Math.random() *GRID_SIZE) + 1,
-    y: Math.floor(Math.random() *GRID_SIZE) +1
+    x: Math.floor(Math.random() * GRID_SIZE) + 1,
+    y: Math.floor(Math.random() * GRID_SIZE) + 1
   }
 }
 
@@ -11,5 +11,5 @@ export function outsideGrid(position) {
   return (
     position.x < 1 || position.x > GRID_SIZE ||
     position.y < 1 || position.y > GRID_SIZE
-    )
+  )
 }
