d56a222a02996b8e1a16936770b42c31693d630c
yxx217 <m-10742662@moe-dl.edu.my>
2024-08-26 16:17:09 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED,
-        getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
 
@@ -7,22 +6,23 @@ let lastRenderTime = 0
 let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
-function main (currentTime) {
+function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press ok to restart.')) {
       window.location = '/'
     }
     return
   }
- 
- window.requestAnimationFrame(main)
- const secondsSinceLastRender = ( currentTime - lastRenderTime) /
-    1000
+
+
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
-lastRenderTime = currentTime
 
-update()
+  lastRenderTime = currentTime
+
+  update()
   draw()
 }
 
